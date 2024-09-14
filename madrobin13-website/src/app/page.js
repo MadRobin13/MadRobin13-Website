9ca46205bc6dccbcf245c7c967dbf48c53dae3dc
MadRobin13 <madrobin1340@gmail.com>
2024-09-14 10:59:14 -0400

    changed to react

--- madrobin13-website/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <Image src="/assets/wallpaperflare.com_wallpaper.jpg" width="100" height="100" alt="sand"/>
-    </div>
-  );
-}
