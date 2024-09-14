777212cb1e62c7cd7b8235eabb0fc8fc2aa57f63
MadRobin13 <madrobin1340@gmail.com>
2024-09-14 17:11:41 -0400

    got depth working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={require("./madrobin_logo_mrk5.png")} className="App-logo" alt="logo" />
       </header>
+
+
+      <img src={require("./sand.jpg")} alt="sand" className="Sand"/>
+
+
+      <body className="App-body">
+        MadRobin13
+      </body>
     </div>
   );
 }
