372a7f69dc221ef4a1670c0cdfe8e71b21411f93
MadRobin13 <madrobin1340@gmail.com>
2024-09-14 22:13:56 -0400

    some more css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={require("./madrobin_logo_mrk5.png")} className="App-logo" alt="logo" />
+        <img src={require("./madrobin_logo_mrk5.png")} className="Logo" alt="logo" />
       </header>
 
-
+      {/* <p>|
+        V</p> */}
       <img src={require("./sand.jpg")} alt="sand" className="Sand"/>
 
-
       <body className="App-body">
-        MadRobin13
+        <div>
+          <h1>Hi, I'm Abhimanyu Chaudhary! <img src={require("./madrobin_logo_mrk5.png")} className="Logo Logo-rotate Logo-title" alt="logo" /></h1>
+        </div>
       </body>
     </div>
   );
