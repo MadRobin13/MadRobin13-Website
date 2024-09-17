8ab9b230f434a0a63c04005a4da5eb94f95f1f0e
MadRobin13 <madrobin1340@gmail.com>
2024-09-16 23:43:06 -0400

    more stuff with triangle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,43 +5,55 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src={require("./madrobin_logo_mrk5.png")} className="Logo" alt="logo"/>
       </header>
-
-      <h1 className="title-settings">Hi, I'm Abhimanyu Chaudhary! <img src={require("./madrobin_logo_mrk5.png")} className="Logo Logo-rotate Logo-title" alt="logo"/></h1>
-      <h2 className='title-settings about-me' id="line-2">I'm a full stack developer with a lot of experience in programming and hardware.</h2>
-      <img src={require("./small_shapes/small_round_square.png")} alt="circle" className='Small-shape'></img>
-      <div id="links">
-        <a href={"https://github.com/MadRobin13"}><img src={require("./github.png")} alt="github logo"/></a>
-        <a href={"mailto:madrobin1340@gmail.com"}><img src={require("./at_symbol_mrk1.png")} alt="email me here button"/></a>
-        <a><img src={require("./linkedin-app-white-icon.png")} alt="linkedin"/></a>
-        <a href={"https://drive.google.com/file/d/1fbJkTsMaUDS1q9sAKar44hsqukhJULHD/view?usp=sharing"}><img src={require("./document-512.png")} alt="resume"/></a>
-        <a href={"https://www.printables.com/@mando13_1132886"}><img src={require("./printables_icon_white_v3.png")} id="printables" alt="printables logo"/></a>
+      <img alt="triangle" id="triangle" src={require("./glow_triangle_v3.png")}/>
+      <div id="title-stuff">
+        <h1 className="title-settings hi">Hi I'm</h1>
+        <h1 className="title-settings" id="name">Abhimanyu Chaudhary.</h1>
+        <h2 className='title-settings about-me' id="line-2">I'm a full stack developer with a lot </h2>
+        <h2 className='title-settings about-me' id="line-3"> of experience in programming and hardware.</h2>
       </div>
 
       <p className='Arrow'>{'>'}</p>
-      <img src={require("./blue_minimal.png")} alt="sand" className="Sand"/>
+      {/* <img src={require("./blue_minimal.png")} alt="sand" className="Sand"/> */}
 
       <body className="App-body">
-        <div>
-          <p id="first-sentence">
-          </p>
-          <p>
-          - I am a programming member and the human player for FRC team <a href={"https://1360.ca/"}>1360 Orbit Robotics</a>
-          </p>
-          <p>
-          - I am an FLL coach for team 60977 as part of the <a href={"https://www.haltonstemclub.ca/"}>Halton Stem Club</a>
-          </p>
-          <p>
-          - I enjoy making projects in my free time
-          </p>
+        <div id="projects">
+          <h2>Projects</h2>
+          <div className="project" id="project-1">
+          <img src={require("./chattr_logo_v5.png")} alt="chattr logo"/>
+            <div>
+              <h3>Project 1</h3>
+              <p>Project 1 Description</p>
+            </div>
+          </div>
+          <div className="project" id="project-2">
+            <h3>Project 2</h3>
+            <p>Project 2 Description</p>
+          </div>
+          <div className="project" id="project-3">
+            <h3>Project 3</h3>
+            <p>Project 3 Description</p>
+          </div>
+          <div className="project" id="project-4">
+            <h3>Project 4</h3>
+            <p>Project 4 Description</p>
+          </div>
         </div>
-        <div id="certification"><a href={"https://www.credly.com/badges/7dc8a2c8-3deb-4af7-b55c-ce09bcd9eb37/public_url"}><img src={require("./ai-and-ml-with-python-skill-circuit-stream-in-partn_small.png")}/></a></div>
-        <p id="disclaimer">Please see my github and resume for more of my projects and experience</p>
       </body>
 
-      {/* <footer className="App-footer"> </footer> */}
+      <footer className="App-footer"> 
+        <div id="links">
+          <a href={"https://github.com/MadRobin13"}><img src={require("./github.png")} alt="github logo"/></a>
+          <a href={"mailto:madrobin1340@gmail.com"}><img src={require("./at_symbol_mrk1.png")} alt="email me here button"/></a>
+          <a><img src={require("./linkedin-app-white-icon.png")} alt="linkedin"/></a>
+          <a href={"https://drive.google.com/file/d/1fbJkTsMaUDS1q9sAKar44hsqukhJULHD/view?usp=sharing"}><img src={require("./document-512.png")} alt="resume"/></a>
+          <a href={"https://www.printables.com/@mando13_1132886"}><img src={require("./printables_icon_white_v3.png")} id="printables" alt="printables logo"/></a>
+        </div>
+      </footer>
       <script>
-        
+
       </script>
     </div>
   );
